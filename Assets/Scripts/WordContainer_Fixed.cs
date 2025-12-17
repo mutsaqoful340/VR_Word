32fb37e55d4ff1c9b00e84bdd321f689e168f338
Fikri Ahmad <rukimevick@gmail.com>
2025-12-17 20:08:20 +0700

    A bit changes before prototyping

--- Assets/Scripts/WordContainer_Fixed.cs
+++ Assets/Scripts/WordContainer_Fixed.cs
@@ -10,10 +10,7 @@ public class WordContainer_Fixed : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"Trigger entered by: {other.gameObject.name}, Tag: {other.tag}");
-        
         // Check if it's the player or a child of the player
-        if (other.CompareTag("Player") || other.transform.root.CompareTag("Player"))
         {
             WordContainerAnim.SetTrigger("PlayerIN");
             Debug.Log("Player entered the word container trigger.");
@@ -22,8 +19,6 @@ public class WordContainer_Fixed : MonoBehaviour
 
     public void OnTriggerExit(Collider other)
     {
-        Debug.Log($"Trigger exited by: {other.gameObject.name}, Tag: {other.tag}");
-        
         // Check if it's the player or a child of the player
         if (other.CompareTag("Player") || other.transform.root.CompareTag("Player"))
         {
