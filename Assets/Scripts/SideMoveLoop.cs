3e1100a1494b6cd10ecf010926becf1a089c54b9
ekosaputra12 <ekosaputra@students.amikom.ac.id>
2026-01-04 14:46:26 +0700

    UpdatePeluru Bisa kena target

--- Assets/Scripts/SideMoveLoop.cs
+++ Assets/Scripts/SideMoveLoop.cs
@@ -6,6 +6,7 @@ public class SideMoveLoop : MonoBehaviour
     public float speed = 1f;          // kecepatan gerak
 
     private Vector3 startPos;
+    private bool isStopped = false;   // status berhenti
 
     void Start()
     {
@@ -14,7 +15,18 @@ public class SideMoveLoop : MonoBehaviour
 
     void Update()
     {
+        if (isStopped) return; // kalau kena peluru, STOP
+
         float offset = Mathf.Sin(Time.time * speed) * moveDistance;
         transform.position = startPos + new Vector3(offset, 0, 0);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // pastikan peluru punya tag "Bullet"
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            isStopped = true;
+        }
     }
 }
