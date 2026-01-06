8cffa56e5e4aa9877a5451dbda4a0440eb7e7888
ekosaputra12 <ekosaputra@students.amikom.ac.id>
2026-01-06 21:53:39 +0700

    Update

---
+++ Assets/Scripts/GARDEN/BoxTrigger.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class BoxTrigger : MonoBehaviour
+{
+    [Header("Box Settings")]
+    public string visualID;
+    public Animator boxAnimator;
+
+    [Header("Feedback")]
+    public AudioSource openSound;
+
+    private bool isOpened = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlantItem plant = other.GetComponent<PlantItem>();
+        if (plant == null) return;
+
+        if (plant.visualID == visualID)
+        {
+            AcceptPlant(other.gameObject);
+        }
+        else
+        {
+            RejectPlant(other.gameObject);
+        }
+    }
+
+    void AcceptPlant(GameObject plant)
+    {
+        if (!isOpened)
+        {
+            isOpened = true;
+
+            if (boxAnimator != null)
+                boxAnimator.SetTrigger("Open");
+
+            if (openSound != null)
+                openSound.Play();
+        }
+
+        Destroy(plant);
+    }
+
+    void RejectPlant(GameObject plant)
+    {
+        Rigidbody rb = plant.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForce(Vector3.up * 2f, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/GARDEN/PlantItem.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class PlantItem : MonoBehaviour
+{
+    [Header("Visual Group ID")]
+    public string visualID;
+    // Contoh: "RED", "BLUE", "STAR"
+}
